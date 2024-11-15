efa8bd21e8e15edf4aae6f55af9698d3f41063fc
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-11-15 02:05:30 -0500

    recoloring

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,16 +1,3 @@
-
-
-const hidesidebar = () => {
-    const chevron = document.getElementById('chevron')
-    document.getElementById('sidebar').classList.toggle('hide')
-    document.getElementById('nav-title').classList.toggle('gap')
-    var on = true
-
-    if (on === true) {
-        chevron.src = "../imgs/chevron.png"
-        on = false
-    } else {
-        chevron.src = "../imgs/chevronright.png"
-        on = true
-    }
-};
+const chevronclick = () => {
+    document.getElementById('nav-container').classList.toggle('hide')
+}
